55b2154becea52afa6f866b638ddfe09fd0c41c4
Abhinay550 <vallamkondaabhi@gmail.com>
2025-02-03 22:17:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+     <div className="App">
+       <h1 style={{color:"YELLOW"}}> VALLAMKONDA ABHINAY</h1>
+    let username = "ABHINAY";
+    return()
+    </div>
+  );
+}
+
+export default App;
